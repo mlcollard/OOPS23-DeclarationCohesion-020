41df647913ad1cd2ee52d9a1123a3756fa339edd
Michael L. Collard <collard@uakron.edu>
2023-03-01 14:54:49 -0500

    Initial commit

---
+++ Cohesion.cpp
@@ -0,0 +1,25 @@
+/*
+    Cohesion.cpp
+
+    Ordering and variable cohesion
+*/
+
+#include <vector>
+#include <string>
+
+int main() {
+
+    // variable ordering & style #1
+    {
+        std::vector<std::string> exerciseNames;
+        std::vector<std::string> testNames;
+
+        std::vector<int> exercises;
+        std::vector<int> tests;
+
+        double exerciseAverage;
+        double testAverage;
+    }
+
+    return 0;
+}
